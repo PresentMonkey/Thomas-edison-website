3646cb434f5798afd7af41c80e3c88050f285f75
PresentMonkey <gscience213@gmail.com>
2021-04-21 22:04:49 -0400

    Initial

---
+++ frontend/src/routes/index.svelte
@@ -0,0 +1,46 @@
+<script>
+	import successkid from 'images/successkid.jpg';
+</script>
+
+<style>
+	h1, figure, p {
+		text-align: center;
+		margin: 0 auto;
+	}
+
+	h1 {
+		font-size: 2.8em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+
+	figure {
+		margin: 0 0 1em 0;
+	}
+
+	img {
+		width: 100%;
+		max-width: 400px;
+		margin: 0 0 1em 0;
+	}
+
+	p {
+		margin: 1em auto;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+</style>
+
+<svelte:head>
+	<title>Virtual Box</title>
+</svelte:head>
+
+<h1>Virtual Box</h1>
+
+
+<p><strong>Login to create a classroom or click <a href="/join">join</a> to join an existing classroom</strong></p>
---
+++ frontend/src/routes/join.svelte
@@ -0,0 +1,31 @@
+<svelte:head>
+    <title>Join</title>
+</svelte:head>
+
+<script>
+    export var joined = false;
+
+
+
+
+
+</script>
+
+
+
+
+<h1>Join</h1>
+
+
+
+{#if joined}
+    <p>You have joined the class!</p>
+    <p>Connect your Virtual Box via usb to the computer and then click connect below</p>
+    <button>Connect</button>
+{:else}
+    <form>
+        <label for="code">Code:  </label><input id="code" type="text"> <br>
+        <label for="name">Name:  </label><input id="name" type="text">  <br>
+        <button type="submit">Join</button> 
+    </form>
+{/if}
